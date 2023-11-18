e91318650dde2e9f68e752630d539cbb3496a34e
MuuSeoTia <148797802+MuuSeoTia@users.noreply.github.com>
2023-11-18 18:13:19 -0500

    Add files via upload
    :bookmark:

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RestaurantList from './restaurantList';
+import CarbonCalculator from './CarbonCalculator';
+import CarbonEmissions from './CarbonEmissions';
+const App = () => {
+  return (
+    
+    <div>
+      < RestaurantList />
+      < CarbonCalculator />
+      < CarbonEmissions />
+    </div>
+  );
+};
+
+export default App;
