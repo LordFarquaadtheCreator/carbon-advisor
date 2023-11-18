d37841d9987ae6c4ebf338fffdd5f0075ad7c83e
AdrianLandaverde <adrianlandaverden@gmail.com>
2023-11-18 17:20:16 -0500

    ✨Route API

--- python-api/app.py
+++ python-api/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 import requests
-import os
+import json
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -21,71 +21,6 @@ app.add_middleware(
 @app.get("/")
 async def hello_world():
     return {'message':'Hello World'}
-
-@app.get("/restaurants")
-async def hello_world():
-    api_key= "AIzaSyAHbJM1jo4wyx5whpMNtfnpsHT30MjJ0JA"
-    url = "https://www.googleapis.com/geolocation/v1/geolocate?key="+api_key
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = {
-        "homeMobileCountryCode": 310,
-        "homeMobileNetworkCode": 410,
-        "radioType": "gsm",
-        "carrier": "Vodafone",
-        "considerIp": True
-    }
-
-    response = requests.post(url, headers=headers, json=data)
-
-    # Check if the request was successful (status code 200)
-    if response.ok:
-        result = response.json()
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-
-    lat= result['location']['lat']
-    lon= result['location']['lng']
-
-    print(lat, lon)
-
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-    params = {
-        'keyword': 'sustainable veggie',
-        'location': f"{lat},{lon}",
-        'radius': 1500,
-        'type': 'restaurant',
-        'key': api_key
-    }
-
-    response = requests.get(url, params=params)
-
-    if response.status_code == 200:
-        # The request was successful
-        data = response.json()
-        # Process the response data as needed
-        print("Ok")
-    else:
-        # There was an error with the request
-        print(f"Error: {response.status_code}, {response.text}")
-
-    print(len(data["results"]))
-
-    address= data['results'][0]['vicinity']
-    name= data['results'][0]['name']
-
-    complete= address + name
-    swap = complete.replace(" ", "+")
-    main_map= f"https://www.google.com/maps/embed/v1/place?key=AIzaSyAHbJM1jo4wyx5whpMNtfnpsHT30MjJ0JA&q={swap}"
-    
-    names= []
-    addresses= []
-    for i in range(len(data["results"])):
-        names.append(data["results"][i]["name"])
-        addresses.append(data["results"][i]["vicinity"])
-
-    return {'names': names, 'addresses': addresses, 'main_map': main_map}
 
 @app.get("/restaurants_fixedlocation")
 def hello_world():
@@ -154,3 +89,40 @@ def hello_world():
         results.append(data["results"][i]["name"] + " " + data["results"][i]["vicinity"])
 
     return {'names': results, 'main_map': main_map}
+
+@app.get("/route")
+async def hello_world(origin: str, destination: str, mode: str, alternatives: bool):
+    api_key= "AIzaSyAHbJM1jo4wyx5whpMNtfnpsHT30MjJ0JA"
+    url = 'https://maps.googleapis.com/maps/api/directions/json'
+    params = {'origin': origin,
+            'destination': destination,
+            'mode': mode,
+            'alternatives': alternatives,
+            'key': api_key}
+
+    response = requests.get(url, params=params)
+    data= json.loads(response.content.decode('utf-8'))
+    n_route=0
+    carbon_emissions=0
+    for i in range(len(data['routes'][n_route]["legs"][0]["steps"])):
+        distance= (data['routes'][n_route]["legs"][0])["steps"][i]["distance"]["value"]
+        travel_mode= (data['routes'][n_route]["legs"][0])["steps"][i]["travel_mode"]
+
+        print("Distance (m):", distance)
+
+        if travel_mode == "TRANSIT":
+            travel_mode= (data['routes'][n_route]["legs"][0])["steps"][i]["transit_details"]["line"]["vehicle"]["name"]
+            print("Mode:", travel_mode)
+        else:
+            print("Mode:", travel_mode)
+
+        distance= distance/1000
+
+        if travel_mode == "DRIVING":
+            carbon_emissions += distance*180
+        elif travel_mode == "Bus":
+            carbon_emissions += distance*75
+        elif travel_mode == "Train":
+            carbon_emissions += distance*15
+
+    return {'carbon_emissions': round(carbon_emissions)}
