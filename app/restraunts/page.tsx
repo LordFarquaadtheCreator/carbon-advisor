fb521ee5944def5bb8cf7a239e3469251d9733ea
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-11-19 02:00:17 -0500

    sustainable resteraunts mvp works!! yippie :rocket: :rocket: :rocket: :rocket:

--- app/restraunts/page.tsx
+++ app/restraunts/page.tsx
@@ -10,7 +10,7 @@ import { DirectionsInputForm } from "../../components/Maps/DirectionsInputForm";
 import GoogleMapDisplay from "../../components/Maps/GoogleMapDisplay";
 import { useDirections } from "@/services/directions";
 import DirectionList from "../../components/Maps/DirectionList";
-
+import "./style.css"
 const NULL_STRING = "---";
 
 export default function RestrauntsPage() {
@@ -26,40 +26,23 @@ export default function RestrauntsPage() {
 
   return (
     <>
+    <h1>
+      Sustainable Restraunts!
+    </h1>
       {/* Direction Web View */}
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <div className="col-span-9">
+      <div className="justify-center items-cent mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <div className="col-span-7 rounded-lg shadow-lg">
           {firstLeg?.end_address ? <RestaurantMap query={firstLeg.end_address} /> : <RestaurantMap query={'Boston University'} />}
         </div>
-      <div className="col-span-3 justify-start">
-            <DirectionsInputForm onSubmit={(formData) => updateDirections(formData)} />
+        {/* Input Form */}
+        <div className="col-span-5  rounded-lg shadow-lg">
+          <DirectionsInputForm onSubmit={(formData) => updateDirections(formData)} />
         </div>
       </div>
 
-      {/* Direction Form */}
-      {firstLeg && (
-        <>
-        <div className="flex flex-row">
-          <DirectionList
-            startAddress={firstLeg.start_address}
-            endAddress={firstLeg.end_address}
-            totalDistance={firstLeg.distance?.text ?? NULL_STRING}
-            totalDuration={firstLeg.duration?.text ?? NULL_STRING}
-            steps={
-              firstLeg.steps.map((step) => {
-                return {
-                  distance: step.distance?.text ?? NULL_STRING,
-                  duration: step.duration?.text ?? NULL_STRING,
-                  // @ts-ignore
-                  htmlInstructions: step["html_instructions"],  // there was an error in API, so we use key-syntax
-                };
-              }) ?? []
-            }
-          />
-          <br/>
-          <RestaurantList endAddress={firstLeg.end_address} />
-        </div>
-        </>
+      {/* Prints Resteraunts */}
+      {firstLeg && (<>
+        <RestaurantList endAddress={firstLeg.end_address} /></>
       )}
     </>
   );
