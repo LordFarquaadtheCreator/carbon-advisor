d4efe2085b235e2a08b7d4007389ce9f3cbedabf
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-11-19 04:08:18 -0500

    unsuccessful

--- components/Maps/DirectionsInputForm.tsx
+++ components/Maps/DirectionsInputForm.tsx
@@ -1,22 +1,69 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useForm, SubmitHandler } from "react-hook-form";
 import axios from 'axios';
 import { DirectionsFormData } from '@/services/directions';
+// import dynamic from 'next/dynamic';
+// import { useEffect, useRef, useState } from 'react';
+// import { useForm } from "react-hook-form";
+import dynamic from 'next/dynamic';
 
-type DirectionsInputFormProps = {
-  onSubmit: SubmitHandler<DirectionsFormData>;
+// Uncomment and adjust the path if needed
+// const DirectionsInputFormWithNoSSR = dynamic(
+//   () => import('@/components/DirectionsInputForm'),
+//   { ssr: false }
+// );
+
+type DirectionsFormData = {
+  // Define your form data structure here
 };
 
-export const DirectionsInputForm: React.FC<DirectionsInputFormProps> = ({ onSubmit }) => {
+type DirectionsInputFormProps = {
+  onSubmit: (data: DirectionsFormData) => void;
+};
+
+const DirectionsInputForm: React.FC<DirectionsInputFormProps> = ({ onSubmit }) => {
+  const [isGoogleMapsLoaded, setIsGoogleMapsLoaded] = useState(false);
   const { register, handleSubmit } = useForm<DirectionsFormData>();
+  const originRef = useRef(null);
+  const destinationRef = useRef(null);
+
+  useEffect(() => {
+    if (window.google && window.google.maps) {
+      setIsGoogleMapsLoaded(true);
+    } else {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places`;
+      script.async = true;
+      script.defer = true;
+      script.onload = () => setIsGoogleMapsLoaded(true);
+      script.onerror = () => console.error('Google Maps API failed to load');
+      document.body.appendChild(script);
+    }
+
+    if (isGoogleMapsLoaded) {
+      const options = { /* your options here */ };
+      const originAutocomplete = new google.maps.places.Autocomplete(originRef.current, options);
+      const destinationAutocomplete = new google.maps.places.Autocomplete(destinationRef.current, options);
+
+      originAutocomplete.addListener("place_changed", () => {
+        const place = originAutocomplete.getPlace();
+        // Handle place change
+      });
+
+      destinationAutocomplete.addListener("place_changed", () => {
+        const place = destinationAutocomplete.getPlace();
+        // Handle place change
+      });
+    }
+  }, [isGoogleMapsLoaded]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div style={{ marginBottom: '10px' }}>
-        <input {...register("origin")} placeholder="Starting Location" style={{ width: '100%', display: 'block', backgroundColor: 'transparent' }} />
+        <input {...register("origin")} ref={originRef} placeholder="Starting Location" style={{ width: '100%', display: 'block', backgroundColor: 'transparent' }} />
       </div>
       <div style={{ marginBottom: '10px' }}>
-        <input {...register("destination")} placeholder="Destination" style={{ width: '100%', display: 'block', backgroundColor: 'transparent' }} />
+        <input {...register("destination")} ref={destinationRef} placeholder="Destination" style={{ width: '100%', display: 'block', backgroundColor: 'transparent' }} />
       </div>
       <div style={{ marginBottom: '10px' }}>
         <select {...register("mode", { required: true })} style={{ width: '100%', display: 'block', backgroundColor: 'transparent' }}>
@@ -30,48 +77,3 @@ export const DirectionsInputForm: React.FC<DirectionsInputFormProps> = ({ onSubm
     </form>
   );
 };
-
-
-// type DirectionsProps = {
-//   origin: string;
-//   destination: string;
-//   mode: string;
-//   apiKey: string;
-//   onDirectionsFetched: (data: any) => void;
-// };
-
-// export const Directions: React.FC<DirectionsProps> = ({ origin, destination, mode, apiKey, onDirectionsFetched }) => {
-//   useEffect(() => {
-//     if (origin && destination && mode) {
-//       getDirections();
-//     }
-//   }, [origin, destination, mode, apiKey]);
-
-//   const getDirections = async () => {
-//     try {
-//       const response = await axios.get(`https://maps.googleapis.com/maps/api/directions/json`, {
-//         params: {
-//           origin,
-//           destination,
-//           mode,
-//           alternatives: true,
-//           key: apiKey,
-//         }
-//       });
-
-//       onDirectionsFetched(response.data);
-//     } catch (error) {
-//       console.error("Error fetching directions:", error);
-//     }
-//   };
-//     const handleFormSubmit = (formData: DirectionsFormData) => {
-//     // Do something with the form data
-//     console.log(formData);
-//   };
-
-// return (
-//   <>
-//     <DirectionsInputForm onSubmit={handleFormSubmit} />
-//   </>
-// );
-// };
