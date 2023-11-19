855c865f7c4ebbf2f589e01bdf11ccfda4961344
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-11-19 03:03:18 -0500

    final styling to Sustainable Restraunts page 💄

--- app/restraunts/page.tsx
+++ app/restraunts/page.tsx
@@ -31,20 +31,23 @@ export default function RestrauntsPage() {
     </h1>
       {/* Direction Web View */}
       <div className="justify-center items-cent mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <div className="col-span-7 rounded-lg shadow-lg">
+        <div className="col-span-7 rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
           {firstLeg?.end_address ? <RestaurantMap query={firstLeg.end_address} /> : <RestaurantMap query={'Boston University'} />}
         </div>
         {/* Input Form */}
-        <div className="col-span-5  rounded-lg shadow-lg">
+        <div className="col-span-5  rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
           <DirectionsInputForm onSubmit={(formData) => updateDirections(formData)} />
         </div>
       </div>
 
       {/* Prints Resteraunts */}
+
       {firstLeg && (<>
-        <RestaurantList endAddress={firstLeg.end_address} /></>
+        <div className='rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark'>
+          <RestaurantList endAddress={firstLeg.end_address} /> 
+        </div>
+      </>    
       )}
-    </>
+    </>    
   );
 }
-
